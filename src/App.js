e35263699ea190671626c2d6ac09f9d5d30bfdfe
Philip Oyetunde <46674349+PhilipRoberto@users.noreply.github.com>
2023-03-10 01:06:04 +0100

    Animate on Scroll Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-function App() {
+export default function App () {
+  useEffect (() => {
+    AOS.init({ duration: 2000});
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App app-container">
+      <div className="aos-heading">
+      <h1>Animate on Scroll (AOS) App</h1>
+      </div>
+      <div className="aos-grids-box-container">
+        <div className="boxes-container">
+          <div data-aos="fade-up" data-aos-duration="3000" className="box box1"></div>
+          <div data-aos="fade-down" data-aos-easing="linear" className="box box2"></div>
+          <div data-aos="flip-right" data-aos-offset="300" className="box box3"></div>
+          <div data-aos="flip-left" className="box box4"></div>
+          <div data-aos="zoom-in" className="box box5"></div>
+          <div data-aos="zoom-out" className="box box6"></div>
+          <div data-aos="fade-up-right" className="box box7"></div>
+          <div data-aos="flip-up-left" className="box box8"></div>
+          <div data-aos="zoom-in-up" className="box box9"></div>
+          <div data-aos="zoom-out-down" className="box box10"></div>
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
